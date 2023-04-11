fef21b5ef5fbd29bc004148a7d7d5328a7710e28
auxoecom <128179308+auxoecom@users.noreply.github.com>
2023-04-10 22:09:47 -0500

    Update content.js

--- content.js
+++ content.js
@@ -5,20 +5,25 @@ let userQuestion = window.prompt("What's your question?");
 let inputField = document.querySelector("input[name='chat-input']");
 if (inputField) {
   inputField.value = userQuestion;
-  
+
   // Submit the form to trigger the chatbot response
   let chatForm = inputField.form;
   chatForm.submit();
+
+  // Wait for the chatbot response to load
+  let chatbotResponse = document.querySelector(".chatbot-response");
+  if (chatbotResponse) {
+    let observer = new MutationObserver(mutations => {
+      // When the chatbot response loads, get the answer and display it to the user
+      let answer = chatbotResponse.querySelector(".response-text");
+      if (answer) {
+        alert(answer.textContent.trim());
+      }
+    });
+    observer.observe(chatbotResponse, { childList: true });
+  } else {
+    console.error("Could not find chatbot response element.");
+  }
 } else {
   console.error("Could not find chat input field.");
 }
-
-// Wait for the chatbot response to load
-let chatbotResponse = document.querySelector(".chatbot-response");
-let observer = new MutationObserver(mutations => {
-  // When the chatbot response loads, get the answer and display it to the user
-  let answer = chatbotResponse.querySelector(".response-text");
-  if (answer) {
-    alert(answer.textContent.trim());
-  }
-});
