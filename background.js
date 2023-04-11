41f5a65e950b31a03a05ef49e035b717a6b9cc61
auxoecom <128179308+auxoecom@users.noreply.github.com>
2023-04-10 22:04:57 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,19 +2,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "getAnswer") {
     let query = encodeURIComponent(request.query);
     let xhr = new XMLHttpRequest();
-    xhr.open("GET","https://app.customgpt.ai/api/projects/909/ask-me-anything/result?prompt=Ask+me+any+Amazon+question \
-    -H 97|8qZOUM1aeHZYwqmNEbyNkPpqkArkw7kZT7AQs5mm \", true);
+    let apiKey = "97|8qZOUM1aeHZYwqmNEbyNkPpqkArkw7kZT7AQs5mm";
+    let apiUrl = "https://app.customgpt.ai/api/projects/909/ask-me-anything/result?prompt=Ask+me+any+Amazon+question";
+    xhr.open("GET", apiUrl, true);
+    xhr.setRequestHeader("Authorization", apiKey);
     xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          let response = JSON.parse(xhr.responseText);
-          sendResponse({answer: response.answer});
-        } else {
-          sendResponse({error: "Error retrieving answer from database."});
-        }
-      }
-    };
-    xhr.send();
+     if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        let response = JSON.parse(xhr.responseText);
+      // handle response here
+    }
+  }
+};
+xhr.send();
     return true;
   }
 });
