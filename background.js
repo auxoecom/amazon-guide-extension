fd0d283757c6995f62c8954fd2d1961f61e8f8ac
auxoecom <128179308+auxoecom@users.noreply.github.com>
2023-04-10 21:51:30 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "getAnswer") {
     let query = encodeURIComponent(request.query);
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", curl https://app.customgpt.ai/api/projects/909/ask-me-anything/result?prompt=Ask+me+any+Amazon+question \
-    -H 'Authorization:97|8qZOUM1aeHZYwqmNEbyNkPpqkArkw7kZT7AQs5mm' \, true);
+    xhr.open("GET","https://app.customgpt.ai/api/projects/909/ask-me-anything/result?prompt=Ask+me+any+Amazon+question \
+    -H 'Authorization:97|8qZOUM1aeHZYwqmNEbyNkPpqkArkw7kZT7AQs5mm' \", true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
